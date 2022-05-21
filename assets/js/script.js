cb9b5d43b43d84e417aa583b566d106802efa5f5
ARJUN <arjunsharman06@gmail.com>
2022-05-20 22:51:20 -0400

    added a check for negative inputs & code clean up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,6 @@
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
 // Assignment code here
-//Object of user password variables 
+
+//Object of userpassword variable 
 var userPassword = {
   password: "",
   length: 0,
@@ -27,7 +12,7 @@ var userPassword = {
 };
 
 //Object of password combination
-var characterArray = [
+var characterObject = [
   {
     "lowerCase": ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
     "upperCase": ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
@@ -50,37 +35,36 @@ function writePassword() {
 //function to generate the password.
 var generatePassword = function generatePassword() {
 
-  //Checking if the length of the password >8 && < 128
+  //Checking if the length of the password > 8 && < 128
   if (validLength()) {
 
     //If the user wants following characters in the password
     charChoice();
 
-    //Creating password
     var i = 0;
 
     //setting the value if the program is run without refreshing the browser
-    userPassword.password ="";
+    userPassword.password = "";
 
     while (i < userPassword.length) {
-      
+
       if (userPassword.isLowerCase) {
-        userPassword.password = userPassword.password + (userPassword.password, characterArray[0].lowerCase[Math.floor(Math.random() * characterArray[0].lowerCase.length)]);
+        userPassword.password = userPassword.password + (userPassword.password, characterObject[0].lowerCase[Math.floor(Math.random() * characterObject[0].lowerCase.length)]);
         i++;
       } if (userPassword.isUpperCase) {
-        userPassword.password = userPassword.password + (userPassword.password, characterArray[0].upperCase[Math.floor(Math.random() * characterArray[0].upperCase.length)]);
+        userPassword.password = userPassword.password + (userPassword.password, characterObject[0].upperCase[Math.floor(Math.random() * characterObject[0].upperCase.length)]);
         i++;
       } if (userPassword.isNumeric) {
-        userPassword.password = userPassword.password + (userPassword.password, characterArray[0].numeric[Math.floor(Math.random() * characterArray[0].numeric.length)]);
+        userPassword.password = userPassword.password + (userPassword.password, characterObject[0].numeric[Math.floor(Math.random() * characterObject[0].numeric.length)]);
         i++;
       } if (userPassword.isSpecialChar) {
-        userPassword.password = userPassword.password + (userPassword.password, characterArray[0].special[Math.floor(Math.random() * characterArray[0].special.length)]);
+        userPassword.password = userPassword.password + (userPassword.password, characterObject[0].special[Math.floor(Math.random() * characterObject[0].special.length)]);
         i++;
       }
     }
   } else {
     window.alert("Thank You for using PassWord Generator..");
-    userPassword.password =  "No password Generated as you selected to quit!!";
+    userPassword.password = "No password Generated as you selected to quit!!";
   }
   return userPassword.password;
 };
@@ -88,29 +72,22 @@ var generatePassword = function generatePassword() {
 //Checking if the user has entered the valid length
 var validLength = function ValidLength() {
 
-  userPassword.length = Number(window.prompt("Input the length of the password"));
+  var userChoice = window.prompt("Input the length of the password");
 
-  //if the user has selected cancel from the prompt
-  if (userPassword.length <= 0) {
+  //user selected "cancel"
+  if (userChoice === null) {
     return false;
-
-    //should not be a character
-  } else if (!/^[0-9]+$/.test(userPassword.length)) {
-    window.alert("Wrong Value Inputted for the password! Try Again")
-    return validLength();
   } else {
-    //Check for the length between the range 8 to 128
-    if (parseInt(userPassword.length) >= 8 && parseInt(userPassword.length) <= 128) {
-      return true;
+    userPassword.length = Number(userChoice);
+
+    //check for line space , alphabets , range btw 8 to 128
+    if (userPassword.length <= 0 || !/^[0-9]+$/.test(userPassword.length) ||
+      !(parseInt(userPassword.length) >= 8 && !parseInt(userPassword.length) <= 128)) {
+      window.alert("Wrong Value Inputted for the password!");
+      window.alert("The password length should be between 8 to 128 characters! Try Again");
+      return validLength();
     } else {
-      window.alert("The password length should be between 8 to 128 characters");
-      var choice = window.confirm("Do you want to continue?");
-      if (choice) {
-        return validLength();
-      }
-      else {
-        return false;
-      }
+      return true;
     }
   }
 };
